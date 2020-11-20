5d074db8ad7bcd27594b87c7aa2bda1bd70759f1
akilaid <45641122+akilaid@users.noreply.github.com>
2020-11-20 18:46:20 +0530

    Update 200OK.sh

--- 200OK.sh
+++ 200OK.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-clear
 echo -e "\e[1m\e[31mHTTP Response check Tool\e[0m"
 echo -e "   \e[97mWritten by \e[36m#RiKuWa\e[0m"
-read -p 'Host list file: ' puka
-read -p 'Output file (Ex:puka.txt): ' sududa
-touch $sududa
+sed -i -e 's/\r$//' $1
+touch $2
 while read LINE; do
 mmmm=$(curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE")
 echo $mmmm
-echo $mmmm >> $sududa
-done < $puka
+echo $mmmm >> $2
+done < $1
