c9a0b4bb79bf7a2071c480b4138d01a9e53be1fd
AKILA INDUNIL <45641122+akilaid@users.noreply.github.com>
2023-04-25 15:10:04 +0530

    Update 200OK.sh

--- 200OK.sh
+++ 200OK.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\e[1m\e[31mHTTP Response check Tool\e[0m"
-echo -e "   \e[97mWritten by \e[36m#RiKuWa\e[0m"
+echo -e "   \e[97mWritten by \e[36makilaid\e[0m"
 sed -i -e 's/\r$//' $1
 touch $2
 while read LINE; do
